8c8f6d6835142e812828eff89ae3b14b6cf96115
bq1990 <bq1990@gmail.com>
2017-01-01 19:37:59 -0600

    add new view

--- private_scale/core/views.py
+++ private_scale/core/views.py
@@ -6,3 +6,8 @@ blueprint = Blueprint('core', __name__, static_folder="../static")
 @blueprint.route('/')
 def home():
     return render_template('home.html')
+
+
+@blueprint.route('/new')
+def new():
+    return render_template('new.html')
--- private_scale/tests/test_views.py
+++ private_scale/tests/test_views.py
@@ -1,4 +1,13 @@
+from flask import url_for
+
+
 def test_home(client):
-    response = client.get('/')
+    response = client.get(url_for('core.home'))
     assert response.status_code == 200
     assert 'Welcome' in str(response.data)
+
+
+def test_new(client):
+    response = client.get(url_for('core.new'))
+    assert response.status_code == 200
+
