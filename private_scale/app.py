0a0b3279badc31f200e9c01c2b67b4107f7727c7
bq1990 <bq1990@gmail.com>
2017-01-02 21:31:44 -0600

    remove comments

--- private_scale/app.py
+++ private_scale/app.py
@@ -1,4 +1,3 @@
-import click
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -11,26 +10,10 @@ debug_toolbar = DebugToolbarExtension()
 
 def create_app(config=prod):
     app = Flask(__name__)
-    # app.config.from_pyfile(config_filename)
     app.config.from_object(config)
-
-    # from yourapplication.model import db
-    # db.init_app(app)
-
     app.register_blueprint(core.views.blueprint)
     db.init_app(app)
     debug_toolbar.init_app(app)
     return app
 
 app = create_app(prod)
-
-
-
-# @manager.command
-# def create_all():
-#     db.create_all()
-
-
-# @manager.command
-# def drop_all():
-#     db.drop_all()
