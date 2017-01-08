c419fcebed49cee748362fe7ab2f45dfa0294633
bq1990 <bq1990@gmail.com>
2017-01-08 16:33:11 -0600

    change db name

--- weigh_in/settings/test.py
+++ weigh_in/settings/test.py
@@ -3,5 +3,5 @@ from .common import *
 DEBUG = False
 TESTING = True
 WTF_CSRF_ENABLED = False  # Allows form testing
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/privatescale_test'
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/weigh_in_test'
 APP_NAME = 'Testing App Name'
