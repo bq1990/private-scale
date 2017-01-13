6449daeca709e360c490b81b5b294cca08c6b631
bq1990 <bq1990@gmail.com>
2017-01-12 22:59:02 -0600

    comment on config

--- weigh_in/settings/common.py
+++ weigh_in/settings/common.py
@@ -5,5 +5,7 @@ DEBUG_TB_INTERCEPT_REDIRECTS = False
 SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/weigh_in'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 APP_NAME = 'Weigh-In'
+# reCaptcha keys for local use only. Create your own at
+# https://www.google.com/recaptcha/intro/
 RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
 RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
