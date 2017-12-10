82d6bcf58ed514448636cfb7719ed8c30f5da403
bq1990 <bq1990@gmail.com>
2017-12-10 15:55:00 -0600

    set up nanobox

--- weigh_in/settings/common.py
+++ weigh_in/settings/common.py
@@ -1,11 +1,17 @@
+import os
+
 DEBUG = True
 WTF_CSRF_SECRET_KEY = 'a random string'
 SECRET_KEY = 'another string'
 DEBUG_TB_INTERCEPT_REDIRECTS = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/weigh_in'
+user = os.environ.get('DATA_DB_USER')
+password  = os.environ.get('DATA_DB_PASS')
+host = os.environ.get('DATA_DB_HOST')
+if host:
+    SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@{}/gonano'.format(user, password, host)
+else:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/weigh_in'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 APP_NAME = 'Weigh-In'
-# reCaptcha keys for local use only. Create your own at
-# https://www.google.com/recaptcha/intro/
-RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
-RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
+RECAPTCHA_PUBLIC_KEY = '6Ld-DBEUAAAAAFlREzQd5IWAfyoc2SHlu5yw-LRm'
+RECAPTCHA_PRIVATE_KEY = '6Ld-DBEUAAAAACJhGf5DsO6gLuqtxDc6Ed56O03q'
--- weigh_in/settings/prod.py
+++ weigh_in/settings/prod.py
@@ -5,11 +5,15 @@ from .common import *
 APP_NAME = os.getenv('APP_NAME', 'Weigh-In')
 DEBUG = False
 
-WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
-SECRET_KEY = os.getenv('SECRET_KEY')
+WTF_CSRF_SECRET_KEY = 'a random string'
+
+# WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY', 'thisisasecretkey')
+SECRET_KEY = os.getenv('SECRET_KEY', 'another secret key')
 RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
 RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+if os.environ.get('DATABASE_URL'):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
